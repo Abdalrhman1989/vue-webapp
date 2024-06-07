5299c8c5eb31a7d7bbc76049d139e2fa887b92e6
Abd Alrhman T Alshaar Dit Darra <abdalrhmanaldarra@gmail.com>
2024-06-07 13:51:47 +0200

    Fix ESLint errors and warnings

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
-const { defineConfig } = require('cypress')
+/* eslint-disable */
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-    supportFile: 'cypress/support/e2e.js',
-  },
-})
+    }
+  }
+});
