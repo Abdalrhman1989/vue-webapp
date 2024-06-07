28de777469641011a53a5550aa48fe399525518f
Abd Alrhman T Alshaar Dit Darra <abdalrhmanaldarra@gmail.com>
2024-06-07 03:50:42 +0200

    Initial commit with Vite and Vitest setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  margin: 0;
+  padding: 20px;
+}
+</style>
